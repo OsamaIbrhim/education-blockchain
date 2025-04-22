659a69d2045c0166abd51b2dd0b11c1903293c8c
OsamaIbrhim <osamaibrhiim@gmail.com>
2025-04-22 05:06:53 +0200

    feat(constants): add IdentityABI and CertificatesABI to abis.ts

--- constants/abis.ts
+++ constants/abis.ts
@@ -9,4 +9,24 @@ export const ExamManagementABI = [
   "function enrollStudent(string memory _examId, address _studentAddress) external",
   "function getExamStatistics(string memory _examId) external view returns (uint256 totalStudents, uint256 averageScore, uint256 passRate)",
   "function getExamResults(string memory _examId) external view returns (address[] memory students, uint256[] memory scores, string[] memory grades)"
-]; 
+];
+export const IdentityABI = [
+  "function registerUser(uint8 _role, string memory _ipfsHash) external",
+  "function verifyUser(address _userAddress) external",
+  "function updateUserIPFS(string memory _newIpfsHash) external",
+  "function getUserRole(address _userAddress) external view returns (uint8)",
+  "function isVerifiedUser(address _userAddress) external view returns (bool)",
+  "function updateUserRole(address _userAddress, uint8 _newRole) external",
+  "function isInstitution(address _address) public view returns (bool)",
+  "function owner() external view returns (address)",
+  "function isAdmin(address _address) external view returns (bool)",
+  "function addAdmin(address _newAdmin) external",
+  "function removeAdmin(address _admin) external"
+];
+
+
+export const CertificatesABI = [
+  "function issueCertificate(address _studentAddress, string memory _ipfsHash) external returns (bytes32)",
+  "function getStudentCertificates(address _student) external view returns (bytes32[])",
+  "function verifyCertificate(bytes32 _certificateId) external view returns (address student, address institution, string ipfsHash, uint256 issuedAt, bool isValid)"
+];
